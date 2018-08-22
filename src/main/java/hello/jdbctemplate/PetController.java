dd0ed496de5d97bebda71225c4f993e99f4b1f04
szlizarrrr <yf8189@gmail.com>
2018-08-22 14:54:49 -0700

    template test

--- src/main/java/hello/jdbctemplate/PetController.java
+++ src/main/java/hello/jdbctemplate/PetController.java
@@ -3,13 +3,18 @@ package hello.jdbctemplate;
 import java.util.List;
 
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Component
 @Path("/pet")
 public class PetController {
@@ -18,10 +23,22 @@ public class PetController {
 	PetDao petDao;
 	
 	@GET
+	@Path("/all")
 	@Produces(MediaType.APPLICATION_JSON_UTF8_VALUE)
-	public List<Pet> getPet() {
-		
-		return petDao.listAllPet();
+	public List<Pet> getAllPet() {
+		return petDao.getAllPet();
+	}
+	
+	@GET
+	@Path("/{id}")
+	@Produces(MediaType.APPLICATION_JSON_UTF8_VALUE)
+	public Pet getPetById(@PathParam("id") Integer id) {
+		return petDao.getPetById(id);
+	}
+	
+	@POST
+	public int addPet(Pet pet) {
+		return petDao.addPet(pet);
 	}
 
 }
--- src/main/java/hello/jdbctemplate/PetDao.java
+++ src/main/java/hello/jdbctemplate/PetDao.java
@@ -4,7 +4,10 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+import org.springframework.jdbc.core.namedparam.SqlParameterSource;
 import org.springframework.stereotype.Repository;
 
 import lombok.extern.slf4j.Slf4j;
@@ -19,8 +22,21 @@ public class PetDao {
 	@Autowired
 	NamedParameterJdbcTemplate namedParameterJdbcTemplate;
 	
-	public List<Pet> listAllPet() {
+	public List<Pet> getAllPet() {
 		return jdbcTemplate.query("SELECT * FROM PET", (rs, rowNum) -> new Pet(rs.getInt("id"), rs.getString("type")));
+	}
+	
+	public Pet getPetById(int id) {
+		log.info("id: {}", id);
+		Pet pet = new Pet();
+		pet.setId(id);
+		SqlParameterSource namedParameters = new BeanPropertySqlParameterSource(pet);
+//		SqlParameterSource namedParameters = new MapSqlParameterSource().addValue("id", id);
+		return namedParameterJdbcTemplate.queryForObject("SELECT * FROM PET WHERE ID = :id", namedParameters, (rs, rowNum) -> new Pet(rs.getInt("id"), rs.getString("type")));
+	}
+	
+	public int addPet(Pet pet) {
+		return jdbcTemplate.update("INSERT INTO PET(type) VALUES(?)", pet.getType());
 	}
 	
 
