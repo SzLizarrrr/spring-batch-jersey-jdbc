87a50bed0fc3d2ea1c73406897ebde57fafab0b1
szlizarrrr <yf8189@gmail.com>
2018-08-26 10:49:09 -0500

    update

--- src/main/java/hello/cxf/CxfClient.java
+++ src/main/java/hello/cxf/CxfClient.java
@@ -5,19 +5,13 @@ import org.apache.cxf.jaxws.endpoint.dynamic.JaxWsDynamicClientFactory;
 
 public class CxfClient {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Exception {
 		JaxWsDynamicClientFactory dcf = JaxWsDynamicClientFactory.newInstance();
 		Client client = dcf.createClient("http://localhost:8080/soap/user?wsdl");
 		Object[] objects;
-		try {
-			objects = client.invoke("getUser", 10002L);
-			System.out.println(objects[0].getClass());
-			System.out.println(objects[0].toString());
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-
+		objects = client.invoke("getUser", 10002L);
+		System.out.println(objects[0].getClass());
+		System.out.println(objects[0].toString());
 	}
 
 }
