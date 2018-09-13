276486a891e276deb727773d659630a439716b1f
szlizarrrr <yf8189@gmail.com>
2018-09-13 16:34:18 -0500

    db to flat file

--- src/main/java/hello/batch/BatchConfiguration.java
+++ src/main/java/hello/batch/BatchConfiguration.java
@@ -15,18 +15,28 @@ import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.repository.support.MapJobRepositoryFactoryBean;
 import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
 import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
+import org.springframework.batch.item.file.FlatFileItemWriter;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
+import org.springframework.batch.item.file.builder.FlatFileItemWriterBuilder;
 import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
+import org.springframework.batch.item.file.transform.BeanWrapperFieldExtractor;
+import org.springframework.batch.item.file.transform.DelimitedLineAggregator;
 import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
+
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @EnableBatchProcessing
+@Slf4j
 public class BatchConfiguration {
 
     @Autowired
@@ -38,11 +48,10 @@ public class BatchConfiguration {
     @Autowired
     BatchDecider batchDecider;
 
-    // tag::readerwriterprocessor[]
     @Bean
-    public FlatFileItemReader<Person> reader() {
+    public FlatFileItemReader<Person> flatReader() {
         return new FlatFileItemReaderBuilder<Person>()
-            .name("personItemReader")
+            .name("personFlatItemReader")
             .resource(new ClassPathResource("sample-data.csv"))
             .delimited()
             .names(new String[]{"firstName", "lastName"})
@@ -51,26 +60,49 @@ public class BatchConfiguration {
             }})
             .build();
     }
+    
+    @Bean
+    public JdbcCursorItemReader<Person> jdbcReader(DataSource dataSource) {
+    	return new JdbcCursorItemReaderBuilder<Person>()
+    			.name("personJdbcItemReader")
+    			.sql("SELECT * FROM PEOPLE")
+    			.dataSource(dataSource)
+    			.saveState(false)
+    			.rowMapper(new PersonMapper())
+    			.build();
+    }
 
     @Bean
     public PersonItemProcessor processor() {
         return new PersonItemProcessor();
     }
+    
+    @Bean
+    public FlatFileItemWriter<Person> flatWriter() {
+    	DelimitedLineAggregator<Person> delLineAgg = new DelimitedLineAggregator<>();
+    	delLineAgg.setDelimiter(", ");
+    	BeanWrapperFieldExtractor<Person> fieldExtractor = new BeanWrapperFieldExtractor<>();
+    	fieldExtractor.setNames(new String[] {"firstName", "lastName"});
+    	delLineAgg.setFieldExtractor(fieldExtractor);
+    	return new FlatFileItemWriterBuilder<Person>()
+    			.name("personFlatItemWriter")
+    			.resource(new FileSystemResource("/tmp/sample-data.dat"))
+    			.lineAggregator(delLineAgg).build();
+
+    }
 
     @Bean
-    public JdbcBatchItemWriter<Person> writer(DataSource dataSource) {
+    public JdbcBatchItemWriter<Person> jdbcWriter(DataSource dataSource) {
         return new JdbcBatchItemWriterBuilder<Person>()
-            .itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
-            .sql("INSERT INTO PEOPLE (first_name, last_name) VALUES (:firstName, :lastName)")
-            .dataSource(dataSource)
-            .build();
+        		.itemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<>())
+        		.sql("INSERT INTO PEOPLE (first_name, last_name) VALUES (:firstName, :lastName)")
+        		.dataSource(dataSource)
+        		.build();
     }
-    // end::readerwriterprocessor[]
 
-    // tag::jobstep[]
     @Bean
     public Job importUserJob(JobCompletionNotificationListener listener, Step step1, Step step2) { // don't know how job know step1 is step1(), step2 is step2(). could be inject by name.
-//    	BatchDecider batchDecider = new BatchDecider();
+    	BatchDecider batchDecider = new BatchDecider();
         return jobBuilderFactory.get("importUserJob")
             .incrementer(new RunIdIncrementer())
             .listener(listener)
@@ -84,23 +116,23 @@ public class BatchConfiguration {
     public Step step1(JdbcBatchItemWriter<Person> writer) {
         return stepBuilderFactory.get("step1")
             .<Person, Person> chunk(10)
-            .reader(reader())
+            .reader(flatReader())
             .processor(processor())
             .writer(writer)
             .build();
     }
     
     @Bean
-    public Step step2(JdbcBatchItemWriter<Person> writer) {
+    public Step step2(JdbcCursorItemReader<Person> reader) {
     	return stepBuilderFactory.get("step2")
     			.<Person, Person> chunk(10)
-                .reader(reader())
+                .reader(reader)
                 .processor(processor())
-                .writer(writer)
+                .writer(flatWriter())
                 .build();
     }
-    // end::jobstep[]
     
+    /******* config for memory save metadata *****/
     @Bean
     public ResourcelessTransactionManager transactionManager() {
         return new ResourcelessTransactionManager();
